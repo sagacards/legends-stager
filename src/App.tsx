6c08345ea18b5bc421e9fa669ab0c302f6e6bbd3
jorgenbuilder <jorgen@buildnode.io>
2022-02-05 16:24:58 -0800

    remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,11 @@ import {
     Route,
     Link
   } from "react-router-dom";
-import SideBySidePage from 'pages/side-by-side';
-import StagerPage from 'pages/stager';
-import AnimatedPage from 'pages/animated';
-import TestPage from 'pages/test';
-import StagingPage from 'pages/new';
+import StagingPage from 'pages/index';
 
 export default function App() {
     return <Router>
         <Routes>
-            {/* <Route path="/stager" element={<StagerPage />} /> */}
-            {/* <Route path="/animated" element={<AnimatedPage />} /> */}
-            {/* <Route path="/test" element={<TestPage />} /> */}
-            {/* <Route path="/" element={<SideBySidePage />} /> */}
             <Route path="/" element={<StagingPage />} />
         </Routes>
     </Router>
